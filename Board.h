6aaf6bb4942be3e8a9877bafebfecc8bc74dc9fa
Lucas Lee <LucasDLee23@gmail.com>
2022-04-24 20:25:25 -0700

    Added win game function

--- Board.h
+++ Board.h
@@ -8,7 +8,8 @@ class Board {
         Board(char p1, char p2); //initialize a board with unique symbols
         char getPosition(int row, int column);
         bool isEmpty();
-        void placeLetter(char letter);
+        bool winGame();
+        void placeLetter(int row, int column, char symbol);
         void printBoard();
         void clearBoard();
     private:
@@ -55,8 +56,70 @@ bool Board::isEmpty() {
     return true;
 }
 
-void Board::placeLetter(char l) {
-    
+bool Board::winGame() {
+    int row = 0;
+    int col = 0;
+    char playerSymbol = board[0][0];
+    for(; row < 3; col++) { //check all row combinations
+        if(col % 3 == 0) {
+            row++;
+            col = 0;
+            playerSymbol = board[row][col];
+        }
+
+        if(playerSymbol != board[row][col]) {
+            return false;
+        }
+    }
+
+    row = 0;
+    col = 0;
+    playerSymbol = board[row][col];
+    for(; col < 3; row++) { //check all column combinations
+        if(row % 3 == 0) {
+            col++;
+            row = 0;
+            playerSymbol = board[row][col];
+        }
+
+        if(playerSymbol != board[row][col]) {
+            return false;
+        }
+    }
+
+    row = 0;
+    col = 0;
+    playerSymbol = board[row][col];
+    for(; col < 3; col++) { //check top-left to bottom-right diagonal
+
+        if(playerSymbol != board[row][col]) {
+            return false;
+        }
+        row++;
+    }
+
+    row = 0;
+    col = 2;
+    playerSymbol = board[row][col];
+    for(; col >= 0; col--) { //check top-right to bottom-left diagonal
+
+        if(playerSymbol != board[row][col]) {
+            return false;
+        }
+
+        row++;
+    }
+
+    return true;
+}
+
+void Board::placeLetter(int row, int column, char symbol) {
+    if(symbol == player1Symbol)
+        board[row][column] = player1Symbol;
+    else if(symbol == player2Symbol)
+        board[row][column] = player2Symbol;
+    else
+        board[row][column] = '/';
 }
 
 void Board::printBoard() {
@@ -76,7 +139,7 @@ void Board::printBoard() {
 void Board::clearBoard() {
     int i = 0;
     int j = 0;
-    while(j <= 3) {
+    while(j < 3) {
         board[j][i] = '/';
         
         i++;
